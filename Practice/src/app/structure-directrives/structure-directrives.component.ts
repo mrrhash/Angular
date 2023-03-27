0e9ae68cf080271624b3b71acec9ab224b24bd9c
mrrhash <hr38359@gmail.com>
2023-03-27 22:40:39 +0500

    if else, for loop, install bootstrap and many more

--- Practice/src/app/structure-directrives/structure-directrives.component.ts
+++ Practice/src/app/structure-directrives/structure-directrives.component.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./structure-directrives.component.css']
 })
 export class StructureDirectrivesComponent {
-  display = true;
+  // display = true;
 
-  display2 = "hassan";
+  // display2 = "hassan";
+
+  // pizza = "fajita";
+
+  //public cars = ["Corolla", "City","land cruisor " , "Alto"]
+
+  //  public persons = [
+  //    { name: "Ali" , age:"21" , isMarried:false},
+  //    { name: "Usman" , age:"54" , isMarried:true},
+  //    { name: "Arshad" , age:"34" , isMarried:true}
+
+  //  ]
+  public persons = [
+    { name: "Ali" , age:"21" , isMarried:false,hobbies:["writing","singing","dancing"]},
+    { name: "Usman" , age:"54" , isMarried:true,hobbies:["songs","singing","dancing"]},
+    { name: "Arshad" , age:"34" , isMarried:true,hobbies:["writing","sports","dancing"]}
+
+  ]
 }
