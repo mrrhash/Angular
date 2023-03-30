927e51b735a1b3acef754c29bbaa95991c3a495c
mrrhash <hr38359@gmail.com>
2023-03-30 19:58:26 +0500

    pipe

--- Practice/src/app/pipe/pipe.component.ts
+++ Practice/src/app/pipe/pipe.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./pipe.component.css']
 })
 export class PipeComponent {
-
+    quote = "Never Give Up!";
+    name = "hassan raza";
+    obj = {
+      name: "hassan raza",
+      age : 20,
+      isMarried : false,
+    }
 }
