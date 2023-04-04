5df04121f104ddf82a85e5f275fb7702b8138237
mrrhash <hr38359@gmail.com>
2023-04-04 23:59:17 +0500

    radio btn dropdown & checkBoxes with reactive-forms

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Form, FormArray, FormControl, FormGroup , Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,46 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'reactive-form';
+  hobbiesArray : string[] = ['Reading','Writing','Dancing','Singing'];
 
   signUpForm = new FormGroup({
-    name : new FormControl(''),
-    age : new FormControl(''),
-    email : new FormControl(''),
+    name : new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    age : new FormControl('',[Validators.required,Validators.min(10),Validators.max(50)]),
+    email : new FormControl('',[Validators.required,Validators.pattern('^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$')]),
+    pass : new FormControl('',[Validators.required,Validators.pattern('(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$')]),
+    gender : new FormControl('',[Validators.required]),
+    country : new FormControl('',[Validators.required]),
+    accept : new FormControl(false,[Validators.requiredTrue]),
+    hobbies : new FormArray([],Validators.required),
   });
+
+  onChange(e:any){
+    console.log(e.target.value,e.target.checked);
+   let checked = e.target.checked;
+   let checkedValue = e.target.value;
+
+   let checkedArray = this.signUpForm.get('hobbies') as FormArray;
+
+   if(checked){
+     checkedArray.push(new FormControl(checkedValue));
+   }
+   else{
+     let i : number = 0;
+     checkedArray.controls.forEach((item)=>{
+       if(item.value === checkedValue){
+         checkedArray.removeAt(i);
+       }
+       i++;
+     })
+   }
+  }
+
   formHandler(){
     console.log(this.signUpForm.value);
+    this.signUpForm.reset();
+
+  }
+  get form(){
+    return this.signUpForm.controls;
   }
 }
