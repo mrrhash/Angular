023157ff1158ea8d7da84cd72d5f2b16b57c206e
mrrhash <hr38359@gmail.com>
2023-04-06 12:01:19 +0500

    page-not-found wildcards & pathMatch

--- AngularRouting/src/app/app-routing.module.ts
+++ AngularRouting/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  //{path:'', component: HomeComponent},// not a good practice
+  //{path:'',redirectTo:'/home', pathMatch:'prefix'},//prefix means just start of the route shohld be empty
+  {path:'',redirectTo:'/home', pathMatch:'full'},//full means when whole route shohld is empty
   {path:'home', component: HomeComponent},
   {path:'about', component: AboutComponent},
-  {path:'contact', component: ContactComponent}
+  {path:'contact', component: ContactComponent},
+  {path:"**" , component: PageNotFoundComponent}//it must be in the end of the route
 ];
 
 @NgModule({
--- AngularRouting/src/app/app.module.ts
+++ AngularRouting/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
